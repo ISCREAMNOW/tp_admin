d73bf78d6976097b4f0b7b92f05533463c0da57e
liuzhiyong <1475633402@qq.com>
2020-09-17 23:24:45 +0800

    这是和数据库表连接的第一步，实例化db对象

--- application/baseinfos/controller/Index.php
+++ application/baseinfos/controller/Index.php
@@ -5,11 +5,17 @@ namespace app\baseinfos\controller;
 
 
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
+    //表名
+    public $table="";
     //读取数据并返回到前端
    public function index(){
+       //实例化数据构造器
+       $db=Db::name($this->table);
+
       return "hello world";
    }
 }
